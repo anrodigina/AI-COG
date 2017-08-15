3d2d01ffe20b398afaef0f077ff85af5365cd32c
anrodigina <anrodigina@gmail.com>
2017-08-15 07:56:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ plt.style.use('ggplot')
 pd.options.display.max_rows = 72
 pd.options.display.max_columns = 72
 file1 = 'psy_20151221.csv'
+file2 = 'NEW.csv'
 data = pd.read_csv(file1, header=0, na_values='?', sep=';', encoding='cp1251', index_col=0)
+data2 = pd.read_csv(file2, header=0, na_values='?', sep=';', encoding='cp1251', index_col=0)
 f1 = open('pearson.txt', 'w')
 f2 = open('kendall.txt', 'w')
 f3 = open('spearman.txt', 'w')
@@ -16,3 +18,6 @@ print(data.corr(method='spearman'), file=f3)
 data.corr(method='pearson').to_csv('pearson.csv', encoding='cp1251')
 data.corr(method='kendall').to_csv('kendall.csv', encoding='cp1251')
 data.corr(method='spearman').to_csv('spearman.csv', encoding='cp1251')
+data2.corr(method='pearson').to_csv('pearson1.csv', encoding='cp1251')
+data2.corr(method='kendall').to_csv('kendall1.csv', encoding='cp1251')
+data2.corr(method='spearman').to_csv('spearman1.csv', encoding='cp1251')
